71e77b9686e5f41ce0eb3c5549529c5c4d6b6c6c
Le Wagon <lewagon@LeWagons-iMac01.local>
2025-03-11 17:51:57 +0200

    created a mood

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.1].define(version: 2025_03_11_100616) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,7 +62,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_11_100616) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 
   add_foreign_key "contents", "categories"
   add_foreign_key "contents", "moods"
